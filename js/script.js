c38a9185168ed5ecc40b136d58eb083652306251
Giosue1994 <giosuelentini1994@outlook.com>
2020-05-28 16:06:36 +0200

    jsnack1

--- js/script.js
+++ js/script.js
@@ -9,11 +9,18 @@ var primoNumero = parseInt(prompt('Inserisci il primo numero'));
 var secondoNumero = parseInt(prompt('Inserisci il secondo numero'));
 
 // viene stampato il numero maggiore
-primoNumero == secondoNumero;
-console.log('i numeri sono uguali');
-
 if (primoNumero > secondoNumero ) {
   console.log('stampa', primoNumero);
 } else if (secondoNumero > primoNumero) {
   console.log('stampa', secondoNumero);
+} else {
+  primoNumero == secondoNumero;
+  console.log('i numeri sono uguali');
 }
+
+
+
+// JSNACK 2
+// L’utente inserisce due parole in successione, con due
+// prompt. Il software stampa prima la parola più corta, poi la
+// parola più lunga.
