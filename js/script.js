48a1520e66dd599e89b714a4a48d590cdc6702a3
Giosue1994 <giosuelentini1994@outlook.com>
2020-05-28 16:18:51 +0200

    jsnack 2

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-// JSNACK 1
-// L’utente inserisce due numeri in successione, con due
-// prompt. Il software stampa il maggiore.
-
-// l'utente inserisce il primo numero
-var primoNumero = parseInt(prompt('Inserisci il primo numero'));
-
-// l'utente inserisce il secondo numero
-var secondoNumero = parseInt(prompt('Inserisci il secondo numero'));
-
-// viene stampato il numero maggiore
-if (primoNumero > secondoNumero ) {
-  console.log('stampa', primoNumero);
-} else if (secondoNumero > primoNumero) {
-  console.log('stampa', secondoNumero);
-} else {
-  primoNumero == secondoNumero;
-  console.log('i numeri sono uguali');
-}
+// // JSNACK 1
+// // L’utente inserisce due numeri in successione, con due
+// // prompt. Il software stampa il maggiore.
+//
+// // l'utente inserisce il primo numero
+// var primoNumero = parseInt(prompt('Inserisci il primo numero'));
+//
+// // l'utente inserisce il secondo numero
+// var secondoNumero = parseInt(prompt('Inserisci il secondo numero'));
+//
+// // viene stampato il numero maggiore
+// if (primoNumero > secondoNumero ) {
+//   console.log('stampa', primoNumero);
+// } else if (secondoNumero > primoNumero) {
+//   console.log('stampa', secondoNumero);
+// } else {
+//   primoNumero == secondoNumero;
+//   console.log('i numeri sono uguali');
+// }
 
 
 
@@ -24,3 +24,21 @@ if (primoNumero > secondoNumero ) {
 // L’utente inserisce due parole in successione, con due
 // prompt. Il software stampa prima la parola più corta, poi la
 // parola più lunga.
+
+// l'utente inserisci la prima parola
+var primaParola = prompt('Inserisci la prima parola');
+
+// l'utente inserisci la seconda parola
+var secondaParola = prompt('Inserisci la seconda parola');
+
+//viene stampata prima la parola più corta poi quella lunga
+if (primaParola.length < secondaParola.length) {
+  console.log('stampa', primaParola);
+  console.log('stampa', secondaParola);
+} else if (secondaParola.length < primaParola.length) {
+  console.log('stampa', secondaParola);
+  console.log('stampa', primaParola);
+} else {
+  primaParola.length = secondaParola.length;
+  console.log('stampa', primaParola, 'e', secondaParola);
+}
